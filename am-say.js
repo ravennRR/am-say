c59ad8fb2c3c54867283ddfadbeaa4f828e4d963
Râvenn <82018720+ravennRR@users.noreply.github.com>
2021-05-14 23:16:35 +0300

    Add files via upload

---
+++ am-say.js
@@ -0,0 +1,10 @@
+client.on("message", async ravennVAJİNAHASTASIYIM => {
+let ravennAMLILAR = "ravennAMLI ravennROL ravennID"
+if (ravennVAJİNAHASTASIYIM.content === "ravennAM-ravennSAY") {
+ravennVAJİNAHASTASIYIM.channel.send(`ravennSunucumuzda ravennToplam ${ravennVAJİNAHASTASIYIM.guild.roles.cache.get(ravennAMLILAR).size} ravennAdet ravennAmlı ravennvVr`)
+return;
+}
+})
+
+
+//tamamen ben yaptım bi kaç türeme çalmış olabilir (!)
